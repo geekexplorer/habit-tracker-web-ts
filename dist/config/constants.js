5db2445556065d6417c5506444264e9924f75ad4
Lenny <lenny.self@gmail.com>
2022-03-08 21:27:40 -0800

    Removed contents of dist folder.

--- dist/config/constants.js
+++
@@ -1,4 +0,0 @@
-export const msInDay = 86400000;
-export const API_URL = "http://elite:5091/api/habit";
-export const FETCH_TIMEOUT_SEC = 10;
-//# sourceMappingURL=constants.js.map
--- dist/views/ModalBaseView.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=ModalBaseView.js.map
