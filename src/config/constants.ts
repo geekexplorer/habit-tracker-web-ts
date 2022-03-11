e17e1abec3cd1cc5caa022fef1f9a4ed3b0e93b1
GeekExplorer <lenny.self@gmail.com>
2022-03-10 18:55:24 -0800

    Updated the API_URL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
 export const msInDay: number = 86400000;
-export const API_URL: string = "http://elite:5091/api/habit";
+export const API_URL: string = "http://budgie-i7:5091/api/habit";
 export const FETCH_TIMEOUT_SEC: number = 10;
